61a44ebaab97d69457de014908e8815abd2529e3
Abdullah Abbas <sl_amg@msn.com>
2024-11-10 20:38:19 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import storeimage from"./istockphoto-874114336-612x612.jpg"     /* to add image 
 function App() {
   return (
 
-
-    <div className="body">    {/* always use classname not class to avoid conflex in future */}
+<body>
+<div className="body">    {/* always use classname not class to avoid conflex in future */}
       {/* the main header */}
       <h1 className="mainHeading">Abdullah's BookStore</h1>
       <h3 className="mainHeading">Best place to Learn</h3>
-      <p className="mainHeading">This is a bookstore created by Abdullah Salah Al Abbas</p>
+      <p className="mainHeading">This is a book store created by Abdullah Salah Al Abbas</p>
       {/* The picture */}
       <img className="imageDim" src={storeimage} alt='Bookstore front picture'></img>   {/* to call picture we use src={-name-} */}
     </div>
+
+
+</body>
+
   );
 }
 

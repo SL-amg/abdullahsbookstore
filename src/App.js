1779540107525655b84716d7500aa2a838eff2de
Abdullah Abbas <sl_amg@msn.com>
2024-11-10 19:41:00 +0300

    Task one using react abdullahs book shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div class="mainbody">
+
+      <h1 class="mainheading">Abdullah's BookStore</h1>
+      <h3 class="mainheading">Best place to Learn</h3>
+      <img class="picture" src='https://media.istockphoto.com/id/874114336/photo/books-store-exterior.jpg?s=612x612&w=0&k=20&c=Mp4Hlr_OHS55IfVAZI3rQPc9Ge3sKk4tI04MsVMogaM=' alt='Bookstore front picture'></img>
     </div>
   );
 }

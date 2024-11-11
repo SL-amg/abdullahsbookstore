a6d97913a6833124cada775af319f6e0125da45f
Abdullah Abbas <sl_amg@msn.com>
2024-11-11 12:33:30 +0300

    completed task #2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 
 import './App.css';
-import storeimage from"./istockphoto-874114336-612x612.jpg"     /* to add image .. add image in src and then write import -name- then from "location" */
+import storeimage from "./istockphoto-874114336-612x612.jpg"     /* to add image .. add image in src and then write import -name- then from "location" */
+import products from './products'; /* to import the array from products.js */
 
 function App() {
   return (
 
-<body>
-<div className="body">    {/* always use classname not class to avoid conflex in future */}
-      {/* the main header */}
-      <h1 className="mainHeading">Abdullah's BookStore</h1>
-      <h3 className="mainHeading">Best place to Learn</h3>
-      <p className="mainHeading">This is a book store created by Abdullah Salah Al Abbas</p>
-      {/* The picture */}
-      <img className="imageDim" src={storeimage} alt='Bookstore front picture'></img>   {/* to call picture we use src={-name-} */}
+
+    <div className="MainPageBody">    {/* always use classname not class to avoid conflex in future */}
+      <div> {/* the main header */}
+        <h1 className="mainHeading">Abdullah's BookStore</h1>
+        <h3 className="mainHeading">Best place to Learn</h3>
+        <p className="mainHeading">This is a book store created by Abdullah Salah Al Abbas as</p>
+      </div>
+      <div>   {/* The picture Div */}
+        <img className="imageDim" src={storeimage} alt='Bookstore front picture'></img>   {/* to call picture we use src={-name-} */}
+      </div>
+
+
+      <div className='ProductsList'> {/* Products Div consisi of 2 main divs one for each item*/}
+
+        <div className='BookContainer'> {/* first book */}
+          <div >{/* first book picture*/}
+            <img className='BookPoster' src={products[0].image} alt ={products[0].name}></img>
+          </div>
+          <div className='BookInfo'>{/* first book details and price*/}
+          <h3 >{products[0].name}</h3>
+          <p >{products[0].price}</p>
+          </div> 
+        </div>
+
+        <div className='BookContainer'> {/* Second book */}
+        <div >{/* second book picture*/}
+            <img className='BookPoster' src={products[1].image} alt ={products[1].name} ></img>
+          </div>
+          <div className='BookInfo'>{/* Second book details and price*/}
+          <h3 >{products[1].name} </h3> {/* to call second one we uses object[ref].name*/}
+          <p >{products[1].price}</p>
+          </div> 
+        </div>
+
+      </div>
+
     </div>
-
-
-</body>
 
   );
 }
---
+++ src/products.js
@@ -0,0 +1,16 @@
+
+
+const products = [
+
+{
+    name:"Twilight Breaking Dawn",
+    price:"10 KD",
+    image:"https://upload.wikimedia.org/wikipedia/en/3/31/Breaking_Dawn_cover.jpg"
+},
+{
+    name:"Charlie and the Chocolate",
+    price:"15 KD",
+    image:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1309211401i/6310.jpg"
+},
+]
+export default products; // to export the array
